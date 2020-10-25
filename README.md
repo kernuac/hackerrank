d109771d6e5ceb0777bee911c36a3f3c7f0182f6
Cristhian Vega Cortes <cristhianv.info@gmail.com>
2020-10-25 18:54:25 -0300

    Passed 6/9

---
+++ README.md
@@ -0,0 +1,4 @@
+# Solving Problems in HackerRank
+
+I'll be uploading code that I've written to solve problems in HackerRank.
+ 
--- between-two-sets/between-two-sets.py
+++ between-two-sets/between-two-sets.py
@@ -6,16 +6,36 @@ import random
 import re
 import sys
 
-def get_total_x( a, b ):
-    pass
+def get_total_x( arr, brr ):
+    arr_result = []
+    result = 0
+    for b in brr :
+        arr_result += get_factors( b )
+    arr_result = list( dict.fromkeys( arr_result ) )
+    
+    for f in arr_result:
+        cnt = 0
+        for i in arr:
+            if f % i == 0:
+                cnt = cnt + 1
+        
+        if cnt == len(arr):
+            result = result + 1
+    
+    return result
+
+def get_factors( element ):
+    return [ x for x in range( 1, element + 1) if element % x == 0 ]
+
 
 if __name__ == '__main__':
-    fptr = open( os.environ['OUTPUT_PATH'], 'w' )
+    #fptr = open( os.environ['OUTPUT_PATH'], 'w' )
     first_multiple_input = input().rstrip().split()
     n = int( first_multiple_input[0] )
     m = int( first_multiple_input[1] )
     arr = list( map ( int, input().rstrip().split() ) )
     brr = list( map ( int, input().rstrip().split() ) )
     total = get_total_x( arr, brr )
-    fptr.write( str( total ) + '\n' )
-    fptr.close()
+    print(total)
+    #fptr.write( str( total ) + '\n' )
+    #fptr.close()
